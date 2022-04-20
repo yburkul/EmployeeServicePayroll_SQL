85d8848214f8ed6a321c6575a7eb0674876f44e0
yburkul <yburkul123@gmail.com>
2022-04-20 18:14:15 +0530

    Added:Retrive data

--- EmployeeServicePayroll.sql
+++ EmployeeServicePayroll.sql
@@ -5,12 +5,12 @@ create table employee_payroll(
 Id int not null primary key,
 Name varchar(50),
 Salary bigint,
-StartDate DateTime);
-
-select * from employee_payroll;
+StartDate Date);
 
 insert into employee_payroll(Id,Name,Salary,StartDate)values
 (1,'Yogesh',30000,'2022-04-20'),
 (2,'Sayali',20000,'2022-02-25'),
 (3,'Arjun',40000,'2022-01-17'),
 (4,'Neha',25000,'2022-04-01');
+
+select * from employee_payroll;
